7560c7cb3e8c753469e6135ddff2b5d83f7a6f07
JuanElNieves <juangonzalezfernandez004@alumnos.ilernasevilla.com>
2023-11-21 10:27:49 +0100

    Animacion profile, foto profile

--- carga.js
+++ carga.js
@@ -15,44 +15,9 @@ function resetPageAnimation() {
           clearInterval(interval);
           document.getElementById('home').scrollIntoView({ behavior: 'instant' });
           document.getElementById('loadingAnimation').style.display = 'none';
-          // Start the reveal animation after the loading is complete
-          startRevealAnimation();
+          
+          
       }
   }, 30);
 }
 
-let animationStarted = false;
-
-function startRevealAnimation() {
-  if (!animationStarted) {
-      animationStarted = true;
-      revealLetters();
-  }
-}
-
-window.addEventListener('scroll', function() {
-  var profileSection = document.getElementById('profile-section');
-  var windowHeight = window.innerHeight;
-  var profileSectionRect = profileSection.getBoundingClientRect();
-
-  // Check if the profile section is in view and the animation hasn't started yet
-  if (!animationStarted && profileSectionRect.top <= windowHeight) {
-      startRevealAnimation();
-  }
-});
-
-function revealLetters() {
-  var spans = document.getElementsByClassName('main__description-span');
-  var delay = 0;
-  var revealRate = 100; // Time in milliseconds between each letter reveal
-
-  for (var i = 0; i < spans.length; i++) {
-      spans[i].style.transition = 'opacity 1s';
-      spans[i].style.opacity = 0;
-      setTimeout(function(span) {
-          span.style.opacity = 1;
-      }, delay, spans[i]);
-
-      delay += revealRate;
-  }
-}
---
+++ progresiontexto.js
@@ -0,0 +1,31 @@
+// Función para dividir texto en caracteres
+function splitText(selector) {
+  const element = document.querySelector(selector);
+  const text = element.innerText;
+  element.innerHTML = ''; // Limpiar el contenido existente
+
+  // Crear span para cada carácter
+  text.split('').forEach(char => {
+    let span = document.createElement('span');
+    span.innerText = char;
+    element.appendChild(span);
+  });
+
+  return Array.from(element.children); // Retorna los elementos span creados
+}
+
+// Uso de la función
+const chars = splitText('.profile-info p');
+
+// Crear la animación con GSAP
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#profile-section",
+    start: "top top",
+    end: "+=150%",
+    pin: true,
+    scrub: 0.50,
+    markers: true,
+  }
+})
+.set(chars, { color: "#ffffff", stagger: 1 }, 0.5);
